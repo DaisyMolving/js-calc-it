55970310e9d3a8c4827f46ad53f79e4f569fa94e
Daisy Molving <daisymolving@gmail.com>
2015-05-06 04:42:59 +1200

    created js file, wrote comments for plan/steps, copied across the code from first calculator project that i thought applicable

---
+++ js/calc.js
@@ -0,0 +1,32 @@
+//define numbers so that when you click one it appears on screen (variables, get id, click event, console.log result on screen)
+
+//make these variables firstNumber, secondNumber)
+
+//attach +, -, /, * buttons to functions    
+
+//this is my code from first calculator project. different input this time -> not prompt but result of event
+    if (basicOperation === "add") {
+         {
+          alert (firstNumber + secondNumber);
+        }
+  } else if (basicOperation === "subtract") {
+         { 
+            alert(firstNumber - secondNumber);
+          }
+  } else if (basicOperation === "multiply") {
+        { 
+        alert(firstNumber * secondNumber);
+      }
+  } else if (basicOperation === "divide") {
+         {
+        alert(firstNumber / secondNumber);
+        }
+  }
+}
+
+//make equation that firstNumber (function of +, -, /, *) secondNumber equals result.
+//event of clicking equals cues result, which prints to screen
+
+//show result on screen
+
+//clear screen, without refreshing. is refreshing the default?
